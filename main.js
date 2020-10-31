1d67a1b10f9f60522234e0bbb30f14836f5b75f9
PedroJanikian <43455987+PedroJanikian@users.noreply.github.com>
2020-10-30 18:14:16 -0600

    Making the list for Galleries

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
     const galleriesURL = "https://www.randyconnolly.com/funwebdev/3rd/api/art/galleries.php";
     
-    
-    document.querySelector('').style.display = 'none';
+    document.querySelector('.lds-spinner').style.display = "inline-block";
+    const mainContent = document.querySelectorAll('.main');
+    for (let m of mainContent) {
+        m.style.display = "none";
+    }
     fetch(galleriesURL)
-        .then( r => r.json())
+        .then( r => r.json() )
         .then( galleries => {
-            document.querySelector('').style.display = 'none';
+            document.querySelector('#spinner1').style.display = 'none';
+            document.querySelector('.list > .main').style.display = "block";
             const galleriesArray = [];
             galleriesArray.push(...galleries);
-            for (let g of galleriesArray) {
-                console.log(g);
-            }
-            console.log(galleriesArray.length)
+            makeGalleriesAtList(galleries);
+            console.log(document.querySelector('#listOfGalleries'));
         })
-        .catch( err => console.error(err));
+        .catch( err => console.error(err) );
+
 });
+
+function makeGalleriesAtList(gals) {
+    for (let g of gals) {
+        let li = document.createElement('li');
+        console.log(g);
+        li.textContent = g.GalleryName;
+        li.value = g.GalleryID;
+        document.querySelector('#listOfGalleries').appendChild(li);
+    }
+}
