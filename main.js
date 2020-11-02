6be108508ad745fc1070eec6f8d94364f3ec31cb
PedroJanikian <43455987+PedroJanikian@users.noreply.github.com>
2020-11-02 02:05:47 -0700

    Modal Fixed

--- main.js
+++ main.js
@@ -130,6 +130,41 @@ document.addEventListener("DOMContentLoaded", function() {
                                         document.querySelector(".singlePainting").style.display = "none";
                                         
                                     });
+
+                                    // Get the modal https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal
+                                    // https://www.w3schools.com/howto/howto_css_modals.asp
+                                    var modal = document.getElementById("myModal");
+
+                                    // Get the button that opens the modal
+                                    var btn = document.getElementById("myBtn");
+
+                                    // Get the <span> element that closes the modal
+                                    var span = document.getElementsByClassName("close")[0];
+
+                                    // When the user clicks the button, open the modal 
+                                    btn.onclick = function() {
+                                        modal.style.display = "block";
+                                        for (let p of paintings) {
+                                            if (p.PaintingID == e.target.value) {
+                                                let img = document.querySelector('.modal-content > img');
+                                                console.log(img);
+                                                img.src = `https://res.cloudinary.com/funwebdev/image/upload/w_700/art/paintings/square/${p.ImageFileName}`;
+
+                                            }
+                                        }
+                                    }
+
+                                    // When the user clicks on <span> (x), close the modal
+                                    span.onclick = function() {
+                                        modal.style.display = "none";
+                                    }
+
+                                    // When the user clicks anywhere outside of the modal, close it
+                                    window.onclick = function(event) {
+                                        if (event.target == modal) {
+                                            modal.style.display = "none";
+                                        }
+                                    }
                                 }
 
                                 
@@ -265,6 +300,7 @@ function createSinglePainting(p) {
 
 
     let img = document.createElement('img');
+    img.id = 'myBtn';
     img.src = `https://res.cloudinary.com/funwebdev/image/upload/w_600/art/paintings/square/${p.ImageFileName}`;
 
 
